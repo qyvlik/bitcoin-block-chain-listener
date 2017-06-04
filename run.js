7d414afd354cfe6ab33185e014b8419bc7b351fe
qyvlik <qyvlik@qq.com>
2017-06-04 20:24:17 +0800

    fix

--- run.js
+++ run.js
@@ -1,29 +1,12 @@
-const BLT = require("./bitcoin-block-chain-listener")
-let bitcoin = new BLT();
+const BitcoinBlockChainListener = require('./bitcoin-block-chain-listener');
 
-bitcoin.connect().catch(function (e) {
-    console.error('listener connect error', e);
+let listener = new BitcoinBlockChainListener();
+listener.connect();
+
+listener.on('tx', function (tx) {
+    console.log('tx', tx.txid);
 });
 
-bitcoin.events.on('tx', function (tx) {
-    console.log("on tx:", tx.txid);
-    // bitcoin.getTx(tx.txid).then(function (txObj) {
-    //     let txId = txObj.txid;
-    //     let vin = txObj.vin;
-    //     let vout = txObj.vout;
-    //     console.log('getTx txid', txId, 'vin length:', vin.length, 'vout length:', vout.length);
-    // }).catch(function (e) {
-    //     console.error(`get tx txId:${tx.txid} fail : error: ${e}`);
-    // });
-    // console.log("typeof tx:", typeof tx);
-    // console.log("keys of tx:", Object.keys(tx));
-    // console.log('txid:', tx.txid);
-    // console.log('vout:', tx.vout);
-    // console.log('vin:', tx.vin);
+listener.on('block', function (block) {
+    console.log('block', block.hash);
 });
-
-bitcoin.events.on('block', function (block) {
-    console.log("block str:", JSON.stringify(block));
-});
-
-console.log('listen run...');
